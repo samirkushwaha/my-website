85fcebd1205676495f7f09acaaee9f282a83d4ac
Sam <isamkushwaha@gmail.com>
2020-12-19 17:24:40 +0530

    Test moreeee

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Samir Kushwaha'),
+      home: MyHomePage(title: 'Hello world!'),
     );
   }
 }
